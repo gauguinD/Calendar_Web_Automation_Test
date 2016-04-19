5f9f0cebdf154c145873f03fe871cca1b35e9f98
gauguind <gauguind@gmail.com>
2016-04-19 21:46:10 +0900

    160419_CalendarTest_2146

--- src/test/java/com/naver/calendar/s04_환경설정/suite_02_환경설정_일정설정_Test.java
+++ src/test/java/com/naver/calendar/s04_환경설정/suite_02_환경설정_일정설정_Test.java
@@ -36,7 +36,7 @@ public class suite_02_환경설정_일정설정_Test extends Testcase {
         util.waitForIsElementPresent(By.xpath("//div[@class='_schedule tc-panel tc-selected']"));
 
         if(util.waitForIsElementPresent(By.xpath("//input[contains(@class,'_cfg_start_day inp01')][contains(@value,'sunday')]")).isSelected()) {
-            util.printLog("현재 한주의 시작은 [일료일] 로 설정되어 있습니다.");
+            util.printLog("현재 한주의 시작은 [일요일] 로 설정되어 있습니다.");
             assertTrue(util.isElementPresent(By.xpath("//input[contains(@class,'_cfg_start_day inp01')][contains(@value,'sunday')]")).isSelected());
         }
         else {
@@ -86,9 +86,9 @@ public class suite_02_환경설정_일정설정_Test extends Testcase {
             if(util.waitForIsElementPresent(By.xpath("//select[@class='_cfg_display_start_idx']/option["+i+"]")).isSelected()){
                 startTime = util.waitForIsElementPresent(By.xpath("//select[@class = '_cfg_display_start_idx']/option["+i+"]")).getText();
                 util.printLog("현재 설정된 시작 시간은 "+startTime);
-
                 assertTrue(util.waitForIsSelected((By.xpath("//select[@class = '_cfg_display_start_idx']/option["+i+"]"))));
-        }
+                break;
+            }
         }
 
         //일정 표시 시간 > 종료시간의 설정값의 개수를 받아옴
@@ -100,10 +100,107 @@ public class suite_02_환경설정_일정설정_Test extends Testcase {
             if(util.waitForIsElementPresent(By.xpath("//select[@class = '_cfg_display_end_idx']/option["+i+"]")).isSelected()){
                 startTime = util.waitForIsElementPresent(By.xpath("//select[@class = '_cfg_display_end_idx']/option["+i+"]")).getText();
                 util.printLog("현재 설정된 종료 시간은 "+startTime);
-
                 assertTrue(util.waitForIsSelected((By.xpath("//select[@class = '_cfg_display_end_idx']/option["+i+"]"))));
+                break;
             }
         }
 
     }
+
+    /*
+    * Step : 일정설정 > 기본시간대 확인
+    * Result : 기본시간대의 현재 값을 확인
+    */
+
+    @Test
+    public void TC_04_일정설정_기본시간대_Test() throws Exception {
+
+        int valueNum = 0;
+        String basicTimeCountry;
+        String basicTimeCity;
+
+        util.waitForIsElementPresent(By.xpath("//div[@class='_schedule tc-panel tc-selected']"));
+
+        //기본시간대 > 기본시간대의 설정값의 개수를 받아옴
+        valueNum = util.getXpathCount(By.xpath("//select[@class='_cfg_default_country_list']/option"));
+
+        //기본시간대 > 기본시간대의 설정값의 개수만큼 for문을 돔
+        for(int i=1; i < valueNum+1; i++ ){
+            //selected된 값이 있으면 그 값의 text를 반환하고 그 값이 select 된지를 assert 함
+            if(util.waitForIsElementPresent(By.xpath("//select[@class='_cfg_default_country_list']/option["+i+"]")).isSelected()){
+                basicTimeCountry = util.waitForIsElementPresent(By.xpath("//select[@class = '_cfg_default_country_list']/option["+i+"]")).getText();
+                util.printLog("현재 설정된 기본시간대 국가는 "+basicTimeCountry);
+                assertTrue(util.waitForIsSelected((By.xpath("//select[@class = '_cfg_default_country_list']/option["+i+"]"))));
+                break;
+            }
+        }
+
+        //기본시간대 > 기본시간대 도시 설정값의 개수를 받아옴
+        valueNum = util.getXpathCount(By.xpath("//select[@class = '_cfg_default_timezone_list']/option"));
+
+        //기본시간대 > 기본시간대 도시 설정값의 개수만큼 for문을 돔
+        for(int i=1; i < valueNum+1; i++ ){
+            //selected된 값이 있으면 그 값의 text를 반환하고 그 값이 select 된지를 assert 함
+            if(util.waitForIsElementPresent(By.xpath("//select[@class = '_cfg_default_timezone_list']/option["+i+"]")).isSelected()){
+                basicTimeCity = util.waitForIsElementPresent(By.xpath("//select[@class = '_cfg_default_timezone_list']/option["+i+"]")).getText();
+                util.printLog("현재 설정된 기본시간대 도시는 "+basicTimeCity);
+                assertTrue(util.waitForIsSelected((By.xpath("//select[@class = '_cfg_default_timezone_list']/option["+i+"]"))));
+                break;
+            }
+        }
+
+    }
+
+    /*
+    * Step : 일정설정 > 추가시간대 확인
+    * Result : 추가시간대의 현재 값을 확인
+    */
+
+    @Test
+    public void TC_05_일정설정_추가시간대_Test() throws Exception {
+
+        int valueNum = 0;
+        String addTimeCountry;
+        String addTimeCity;
+
+        util.waitForIsElementPresent(By.xpath("//div[@class='_schedule tc-panel tc-selected']"));
+
+        //추가시간대 > 추가시간대의 설정값의 개수를 받아옴
+        valueNum = util.getXpathCount(By.xpath("//select[@class='_cfg_default_country_list']/option"));
+
+        //추가시간대 > 추시간대의 설정값의 개수만큼 for문을 돔
+        for(int i=1; i < valueNum+1; i++ ){
+            //selected된 값이 있으면 그 값의 text를 반환하고 그 값이 select 된지를 assert 함
+            if(util.waitForIsElementPresent(By.xpath("//select[@class='_cfg_additional_country_list']/option["+i+"]")).isSelected()){
+                addTimeCountry = util.waitForIsElementPresent(By.xpath("//select[@class = '_cfg_additional_country_list']/option["+i+"]")).getText();
+                util.printLog("현재 설정된 추가시간대 국가는 "+addTimeCountry);
+                assertTrue(util.waitForIsSelected((By.xpath("//select[@class = '_cfg_additional_country_list']/option["+i+"]"))));
+                break;
+            }
+            else if(!(util.isElementPresent(By.xpath("//select[@class = '_cfg_additional_country_list']/option["+i+"]"))).isSelected()){
+                util.printLog("현재 설정된 추가시간대는 없습니다");
+                assertTrue(!(util.isElementPresent(By.xpath("//select[@class = '_cfg_additional_country_list']/option["+i+"]"))).isSelected());
+            }
+
+        }
+
+        //추가시간대 > 추가시간대 도시의 설정값의 개수를 받아옴
+        valueNum = util.getXpathCount(By.xpath("//select[@class = '_cfg_additional_timezone_list']/option"));
+
+        //추가시간대 > 추가시간대 도시의 설정값의 개수만큼 for문을 돔
+        for(int i=1; i < valueNum+1; i++ ){
+            //selected된 값이 있으면 그 값의 text를 반환하고 그 값이 select 된지를 assert 함
+            if(util.waitForIsElementPresent(By.xpath("//select[@class = '_cfg_additional_timezone_list']/option["+i+"]")).isSelected()){
+                addTimeCity = util.waitForIsElementPresent(By.xpath("//select[@class = '_cfg_additional_timezone_list']/option["+i+"]")).getText();
+                util.printLog("현재 설정된 추가시간대 도시는 "+addTimeCity);
+                assertTrue(util.waitForIsSelected((By.xpath("//select[@class = '_cfg_additional_timezone_list']/option["+i+"]"))));
+            }
+            else if(!(util.isElementPresent(By.xpath("//select[@class = '_cfg_additional_timezone_list']/option["+i+"]"))).isSelected()){
+                util.printLog("현재 설정된 추가시간대는 없습니다");
+                assertTrue(!(util.isElementPresent(By.xpath("//select[@class = '_cfg_additional_timezone_list']/option["+i+"]"))).isSelected());
+            }
+        }
+
+    }
+
 }
